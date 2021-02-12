61e682005fc99384f0f7e8b68609c6e3133d6269
pihart <avi.mehra1@gmail.com>
2021-02-12 15:38:49 -0800

    Cap health in receiveDamage

--- src/player.ts
+++ src/player.ts
@@ -44,7 +44,7 @@ export default class Player {
    * @return Whether still alive
    */
   receiveDamage = (damage: number) => {
-    this.health -= damage;
+    this.health = Math.min(this.MaxHealth, Math.max(0, this.health - damage));
     return this.health > 0;
   };
 
