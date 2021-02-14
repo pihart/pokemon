fe38ec42b6efc081b252a9c8eef75e4eb59ff674
pihart <avi.mehra1@gmail.com>
2021-02-14 00:14:44 -0800

    Revert init

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
-const init = require("./init");
+const { Team, Player, Players, Game } = require(".");
+
 console.time();
-console.log(init());
+const A = new Team([new Player(Players.Weedle, true)]);
+const B = new Team([
+  new Player(Players.Gengar1, false),
+  new Player(Players.Golbat, false),
+  new Player(Players.Haunter, false),
+  new Player(Players.Arbok, false),
+  new Player(Players.Gengar2, false),
+]);
+
+console.log(new Game(A, B).play);
 console.timeEnd();
