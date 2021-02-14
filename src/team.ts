7169ddb433cbb9f91abedbfeebebb3342a912cc7
pihart <avi.mehra1@gmail.com>
2021-02-13 17:54:40 -0800

    Override terminate behavior if swapped

--- src/team.ts
+++ src/team.ts
@@ -3,6 +3,15 @@ import Player from "./player";
 
 class Team {
   public currentPlayer = 0;
+  /**
+   * The index of the player that the current player is swapped with, if any.
+   *
+   * There is only ever one swapped pair;
+   * a member of a swapped pair dying means that neither member of the pair is swapped anymore.
+   * This also means that there can be no background swapped pairs:
+   * if the active player is not in a swapped pair, then nobody else is either.
+   */
+  private swappedPlayer?: number;
 
   constructor(private readonly players: Readonly<NonEmptyArray<Player>>) {}
 
@@ -34,6 +43,13 @@ class Team {
    * @return Whether the game is still active
    */
   private terminatePlayer() {
+    if (this.swappedPlayer !== undefined) {
+      this.currentPlayer = this.swappedPlayer;
+      this.swappedPlayer = undefined;
+      // swapped player is always alive, by definition
+      return true;
+    }
+
     const { length } = this.players;
 
     for (let offset = 1; offset < length; offset++) {
