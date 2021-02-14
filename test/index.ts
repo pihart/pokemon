b1731ecc31c6170c0329ee43ef6bb75657c93480
pihart <avi.mehra1@gmail.com>
2021-02-14 01:35:36 -0800

    Add test

---
+++ test/index.ts
@@ -0,0 +1,24 @@
+import { Player, Players, Team, Game } from "..";
+import { Assert } from "@mehra/ts";
+
+for (let file of ["./success/1.json"]) {
+  const rands = require(file);
+  let i = 0;
+  function random() {
+    return rands[i++];
+  }
+
+  const A = new Team([new Player(Players.Weedle, true, random)], random);
+  const B = new Team(
+    [
+      new Player(Players.Gengar1, false, random),
+      new Player(Players.Golbat, false, random),
+      new Player(Players.Haunter, false, random),
+      new Player(Players.Arbok, false, random),
+      new Player(Players.Gengar2, false, random),
+    ],
+    random
+  );
+
+  Assert(new Game(A, B, random).play());
+}
