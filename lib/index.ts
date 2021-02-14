b54d6d5cb20d77f6789f5bb7084c84597f5d67b9
pihart <avi.mehra1@gmail.com>
2021-02-13 23:27:53 -0800

    Only give relevant exports

--- lib/index.ts
+++ lib/index.ts
@@ -1,6 +1,5 @@
 export { default as Game } from "./game";
-export * as Move from "./moves";
-export * as Player from "./players";
-export { default as Resistances } from "./resistances";
 export { default as Team } from "./team";
-export { default as Type } from "./type";
+export { default as Player } from "./player";
+
+export * as Players from "./players";
