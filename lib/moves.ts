5144de5cfcaf2e1a5f43c34a15c2a54fc082916f
pihart <avi.mehra1@gmail.com>
2021-02-13 22:46:44 -0800

    Change export behavior of moves.ts

--- lib/moves.ts
+++ lib/moves.ts
@@ -1,142 +1,138 @@
 import Type from "./type";
 import { Move } from "./move";
 
-const Moves: { [index: string]: Move } = {
-  ConfuseRay: {
-    AttackStat: 0,
-    Type: Type.Ghost,
-    isSpecial: false,
-    execute: (actor, recipient) => {
-      if (Math.random() < 0.75) recipient.confuse(actor);
-      return true;
-    },
-  },
-  NightShade: {
-    AttackStat: 0,
-    Type: Type.Ghost,
-    isSpecial: false,
-    execute: (actor, recipient) => recipient.receiveDamage(actor.Level),
-  },
-  DreamEater: {
-    AttackStat: 100,
-    Type: Type.Psychic,
-    isSpecial: true,
-    execute: (actor, recipient) => {
-      if (recipient.sleepingTurnsLeft && Math.random() < 255 / 256)
-        return recipient.receiveDamagingMove(Moves.DreamEater, actor);
-      return true;
-    },
-  },
-  WingAttack: {
-    AttackStat: 35,
-    Type: Type.Flying,
-    isSpecial: false,
-    execute: (actor, recipient) => {
-      if (Math.random() < 255 / 256)
-        return recipient.receiveDamagingMove(Moves.WingAttack, actor);
-      return true;
-    },
-  },
-  Bite: {
-    AttackStat: 60,
-    Type: Type.Normal,
-    isSpecial: false,
-    execute: (actor, recipient) => {
-      if (Math.random() < 255 / 256)
-        return recipient.receiveDamagingMove(Moves.Bite, actor);
-      return true;
-    },
-  },
-  Acid: {
-    AttackStat: 40,
-    Type: Type.Poison,
-    isSpecial: true,
-    execute: (actor, recipient) => {
-      if (Math.random() < 255 / 256) {
-        if (Math.random() < 0.332) {
-          recipient.adjustStage(-1, "AttackStage", "Normal");
-        }
-        return recipient.receiveDamagingMove(Moves.Acid, actor);
-      }
-      return true;
-    },
-  },
-  Hypnosis: {
-    AttackStat: 0,
-    Type: Type.Psychic,
-    isSpecial: true,
-    execute: (actor, recipient) => {
-      if (Math.random() < 0.6) recipient.makeSleep();
-      return true;
-    },
-  },
-  Supersonic: {
-    AttackStat: 0,
-    Type: Type.Normal,
-    isSpecial: true,
-    execute: (actor, recipient) => {
-      if (Math.random() < 0.55) recipient.confuse(actor);
-      return true;
-    },
-  },
-  Screech: {
-    AttackStat: 0,
-    Type: Type.Normal,
-    isSpecial: true,
-    execute: (actor, recipient) => {
-      if (Math.random() < 0.85) {
-        recipient.adjustStage(-2, "AttackStage", "Normal");
-      }
-      return true;
-    },
-  },
-  Glare: {
-    AttackStat: 0,
-    Type: Type.Normal,
-    isSpecial: true,
-    execute: (actor, recipient) => {
-      if (Math.random() < 0.75) recipient.paralyze();
-      return true;
-    },
-  },
-  PoisonSting: {
-    AttackStat: 15,
-    Type: Type.Poison,
-    isSpecial: false,
-    execute: (actor, recipient) => {
-      if (Math.random() < 255 / 256) {
-        if (Math.random() < 0.2) {
-          recipient.poison();
-        }
-        return recipient.receiveDamagingMove(Moves.PoisonSting, actor);
-      }
-      return true;
-    },
-  },
-  Toxic: {
-    AttackStat: 0,
-    Type: Type.Poison,
-    isSpecial: false,
-    execute: (actor, recipient) => {
-      if (Math.random() < 0.85) recipient.poison();
-      return true;
-    },
-  },
-  Haze: {
-    AttackStat: 0,
-    Type: Type.Ice,
-    isSpecial: true,
-    execute: (actor, recipient) => {
-      if (Math.random() < 255 / 256) {
-        recipient.unConfuse();
-        actor.forceResetStages();
-        recipient.forceResetStages();
-        actor.waiveParalysisSpeedEffect();
-        recipient.waiveParalysisSpeedEffect();
-      }
-      return true;
-    },
+export const ConfuseRay: Move = {
+  AttackStat: 0,
+  Type: Type.Ghost,
+  isSpecial: false,
+  execute: (actor, recipient) => {
+    if (Math.random() < 0.75) recipient.confuse(actor);
+    return true;
   },
 };
-
-export default Moves;
+export const NightShade: Move = {
+  AttackStat: 0,
+  Type: Type.Ghost,
+  isSpecial: false,
+  execute: (actor, recipient) => recipient.receiveDamage(actor.Level),
+};
+export const DreamEater: Move = {
+  AttackStat: 100,
+  Type: Type.Psychic,
+  isSpecial: true,
+  execute: (actor, recipient) => {
+    if (recipient.sleepingTurnsLeft && Math.random() < 255 / 256)
+      return recipient.receiveDamagingMove(DreamEater, actor);
+    return true;
+  },
+};
+export const WingAttack: Move = {
+  AttackStat: 35,
+  Type: Type.Flying,
+  isSpecial: false,
+  execute: (actor, recipient) => {
+    if (Math.random() < 255 / 256)
+      return recipient.receiveDamagingMove(WingAttack, actor);
+    return true;
+  },
+};
+export const Bite: Move = {
+  AttackStat: 60,
+  Type: Type.Normal,
+  isSpecial: false,
+  execute: (actor, recipient) => {
+    if (Math.random() < 255 / 256)
+      return recipient.receiveDamagingMove(Bite, actor);
+    return true;
+  },
+};
+export const Acid: Move = {
+  AttackStat: 40,
+  Type: Type.Poison,
+  isSpecial: true,
+  execute: (actor, recipient) => {
+    if (Math.random() < 255 / 256) {
+      if (Math.random() < 0.332) {
+        recipient.adjustStage(-1, "AttackStage", "Normal");
+      }
+      return recipient.receiveDamagingMove(Acid, actor);
+    }
+    return true;
+  },
+};
+export const Hypnosis: Move = {
+  AttackStat: 0,
+  Type: Type.Psychic,
+  isSpecial: true,
+  execute: (actor, recipient) => {
+    if (Math.random() < 0.6) recipient.makeSleep();
+    return true;
+  },
+};
+export const Supersonic: Move = {
+  AttackStat: 0,
+  Type: Type.Normal,
+  isSpecial: true,
+  execute: (actor, recipient) => {
+    if (Math.random() < 0.55) recipient.confuse(actor);
+    return true;
+  },
+};
+export const Screech: Move = {
+  AttackStat: 0,
+  Type: Type.Normal,
+  isSpecial: true,
+  execute: (actor, recipient) => {
+    if (Math.random() < 0.85) {
+      recipient.adjustStage(-2, "AttackStage", "Normal");
+    }
+    return true;
+  },
+};
+export const Glare: Move = {
+  AttackStat: 0,
+  Type: Type.Normal,
+  isSpecial: true,
+  execute: (actor, recipient) => {
+    if (Math.random() < 0.75) recipient.paralyze();
+    return true;
+  },
+};
+export const PoisonSting: Move = {
+  AttackStat: 15,
+  Type: Type.Poison,
+  isSpecial: false,
+  execute: (actor, recipient) => {
+    if (Math.random() < 255 / 256) {
+      if (Math.random() < 0.2) {
+        recipient.poison();
+      }
+      return recipient.receiveDamagingMove(PoisonSting, actor);
+    }
+    return true;
+  },
+};
+export const Toxic: Move = {
+  AttackStat: 0,
+  Type: Type.Poison,
+  isSpecial: false,
+  execute: (actor, recipient) => {
+    if (Math.random() < 0.85) recipient.poison();
+    return true;
+  },
+};
+export const Haze: Move = {
+  AttackStat: 0,
+  Type: Type.Ice,
+  isSpecial: true,
+  execute: (actor, recipient) => {
+    if (Math.random() < 255 / 256) {
+      recipient.unConfuse();
+      actor.forceResetStages();
+      recipient.forceResetStages();
+      actor.waiveParalysisSpeedEffect();
+      recipient.waiveParalysisSpeedEffect();
+    }
+    return true;
+  },
+};
