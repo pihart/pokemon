c62cf251d0fd4d2075c99c8493377dfaa77c8e81
pihart <avi.mehra1@gmail.com>
2021-02-15 21:51:13 -0800

    Remove dist/ again

--- dist/index.d.ts
+++
@@ -1,4 +0,0 @@
-export { default as Game } from "./game";
-export { default as Team } from "./team";
-export { default as Player } from "./player";
-export * as Players from "./players";
--- dist/resistances.d.ts
+++
@@ -1,4 +0,0 @@
-import Type from "./type";
-import { InvertedMatrix } from "@mehra/ts";
-export declare const Resistances: InvertedMatrix<Type, "weakTo" | "strongTo" | "immuneTo">;
-export default Resistances;
